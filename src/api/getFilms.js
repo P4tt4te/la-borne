ef586d5c23d9de45e0921143d38c74d8975c21a0
sienning <sienning@hotmail.fr>
2022-12-06 16:27:10 +0100

    fetch api filmls

--- src/api/getFilms.js
+++ src/api/getFilms.js
@@ -1,15 +1,18 @@
 import { apiKey } from "./constants";
+import axios from 'axios';
 
 export const getFilms = (type) => {
   const page = "1";
-  const options = { method: "GET" };
+  // const options = { method: "GET" };
 
   // type : popular | top_rated
-  fetch(
+  return axios.get(
     `https://api.themoviedb.org/3/movie/${type}?api_key=${apiKey}&language=fr-FR&page=${page}`,
-    options
   )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
+    // .then((response) => console.log(response))
+    .then((response) => {
+      // console.log(response.data);
+      return response.data
+    })
     .catch((err) => console.error(err));
 };
