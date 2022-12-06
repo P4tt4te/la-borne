6aec5fa53e8bd4f6f3712cefc8630b541b0635f9
Edward B <57622377+P4tt4te@users.noreply.github.com>
2022-12-06 21:55:22 +0100

    init R3F

--- src/components/views/Theater.jsx
+++ src/components/views/Theater.jsx
@@ -1,13 +1,55 @@
-import React from "react";
+import * as THREE from "three";
+import React, { Suspense, useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import "../styles/Theater.css";
+import {
+  Html,
+  Environment,
+  useGLTF,
+  ContactShadows,
+  OrbitControls,
+} from "@react-three/drei";
 
-const Theater = ({
-    handleChangeView
-}) => {
-    return (
-        <div>
-           Theater 
-        </div>
-    )
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef();
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
 }
+
+const Theater = ({ handleChangeView }) => {
+  const group = useRef();
+
+  return (
+    <div className="theater">
+      <Canvas camera={{ position: [-5, 0, -15], fov: 55 }}>
+        <pointLight position={[10, 10, 10]} intensity={1.5} />
+        <Box position={[0, 0, 0]} />
+        <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} />
+        <OrbitControls
+          enablePan={false}
+          enableZoom={false}
+          minPolarAngle={Math.PI / 2.2}
+          maxPolarAngle={Math.PI / 2.2}
+        />
+      </Canvas>
+    </div>
+  );
+};
 
 export default Theater;
