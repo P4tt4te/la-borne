260278623cd614cf141da32ede71c948b3bdb93c
sienning <sienning@hotmail.fr>
2022-12-06 11:54:13 +0100

    movie cards

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MovieCard = () => {
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                Affiche
+            </div>
+            <div className="movie-title">
+                Titre
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard;
