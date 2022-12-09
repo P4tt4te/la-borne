2a916bd693b9311f26e86c17a2c993cf1603e9dc
Edward B <57622377+P4tt4te@users.noreply.github.com>
2022-12-09 14:03:28 +0100

    fix: remove axios

--- src/api/getFilms.js
+++ src/api/getFilms.js
@@ -1,18 +1,14 @@
 import { apiKey } from "./constants";
-import axios from 'axios';
 
 export const getFilms = (type, page) => {
   // const page = "1";
-  // const options = { method: "GET" };
+  const options = { method: "GET" };
 
-  // type : popular | top_rated
-  return axios.get(
+  return fetch(
     `https://api.themoviedb.org/3/movie/${type}?api_key=${apiKey}&language=fr-FR&page=${page}`,
+    options
   )
-    // .then((response) => console.log(response))
-    .then((response) => {
-      // console.log(response.data);
-      return response.data
-    })
+    .then((response) => response.json())
+    .then((response) => response)
     .catch((err) => console.error(err));
 };
